2881a7594b39f0de3badef9cba44c9e418d86c9e
Jasal Vadgama <jasal@blacklabelcreative.com>
2019-09-19 14:07:08 +0100

    adding hosted code

---
+++ tplcss/script.js
@@ -0,0 +1,35 @@
+// /*jslint eqeqeq: true, undef: true */
+// /*global $, window, console, alert */
+
+// var SHELL = SHELL || {};
+
+// SHELL = (function() {
+//     // PRIVATE VARIABLES
+//     // var
+//     // PRIVATE FUNCTIONS
+//     // ;
+
+//     // PUBLIC METHODS
+//     return {
+//         init: function() {
+//             // DOM ready
+
+//             console.log('mooooo');
+//         },
+//         pageInit: function() {
+//             // page load
+//         }
+//     };
+// }());
+
+// // ON DOM READY
+// $(function() {
+//     SHELL.init();
+// });
+
+// // ON PAGE LOAD
+// $(window).load(function() {
+//     SHELL.pageInit();
+// });
+
+console.log('mooooo');
